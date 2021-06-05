8aa82b821fb515c76f160b44e78dd206b8a55778
andremonteiro95 <andremonteiro95@gmail.com>
2021-06-05 16:49:41 +0100

    padding card

--- src/components/projects/NewProjectGridItem.jsx
+++ src/components/projects/NewProjectGridItem.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardContent,
@@ -23,23 +24,30 @@ function NewProjectGridItem() {
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card>
-        <CardContent className={classes.cardContent}>
-          <Typography textAlign="center" variant="h5" component="h3">
-            Create a new project
-          </Typography>
-          <form>
-            <TextField
-              fullWidth
-              margin="normal"
-              label="Project name"
-              variant="outlined"
-              size="small"
-            />
-            <Button type="submit" fullWidth variant="contained" color="primary">
-              Create Project
-            </Button>
-          </form>
-        </CardContent>
+        <Box marginY={4}>
+          <CardContent className={classes.cardContent}>
+            <Typography textAlign="center" variant="h5" component="h3">
+              Create a new project
+            </Typography>
+            <form>
+              <TextField
+                fullWidth
+                margin="normal"
+                label="Project name"
+                variant="outlined"
+                size="small"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+              >
+                Create Project
+              </Button>
+            </form>
+          </CardContent>
+        </Box>
       </Card>
     </Grid>
   );
